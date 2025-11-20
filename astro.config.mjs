6b272b6e4da4b1a3e71d4686aa731b7b86a73ebe
Oluwaseyi (Ekpere) Oyewunmi <thoth2357@gmail.com>
2025-11-20 01:13:23 -0800

    :pencil2: temp move to github pages for now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://seyidev.tech",
+	site: "https://thoth2357.github.io/portfolio",
 	integrations: [
 		mdx(),
 		sitemap(),
